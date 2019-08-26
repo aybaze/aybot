13ca727ba4efe49c5a4ca3ab651e6ca3051d80a7
Andreas <andreas.holzapfel@gmail.com>
2019-08-27 00:49:44 +0200

    Update main.go

--- main.go
+++ main.go
@@ -286,7 +286,7 @@ func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
 	}
 
 	for _, channel := range event.Guild.Channels {
-		if channel.Name == "hampel" {
+		if channel.Name == "naughtyfications" {
 			_, _ = s.ChannelMessageSend(channel.ID, "Aybot is ready to serve.")
 			homeChannels[event.Guild.ID] = channel.ID
 			return
